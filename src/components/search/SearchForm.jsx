e3ed9379bc2016974f1be5d6574460c9898735de
Andy Nguyen <82324179+dquangxyz@users.noreply.github.com>
2023-06-23 11:01:50 +0700

    click enter on keyboard and search function will be triggered

--- src/components/search/SearchForm.jsx
+++ src/components/search/SearchForm.jsx
@@ -4,16 +4,25 @@ import './SearchForm.css'
 const SearchForm = (props) => {
     const [query, setQuery] = useState("")
 
-    //Handler functions
-    const handlerChange = (e) => {
+    // Handler functions
+    const handleChange = (e) => {
         setQuery(e.target.value)
     }
-    const handlerClickReset = () => {
+
+    const handleClickReset = () => {
         setQuery("")
         props.onReceiveQuery("")
     }
-    const handlerClickSearch = () => {
+
+    const handleClickSearch = () => {
         props.onReceiveQuery(query)
+    }
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            e.preventDefault(); // Prevent form submission
+            handleClickSearch();
+        }
     }
 
     return (
@@ -23,21 +32,12 @@ const SearchForm = (props) => {
                     <input
                         type='text'
                         placeholder='Type Keywords'
-                        onChange={handlerChange}
+                        onChange={handleChange}
+                        onKeyDown={handleKeyDown}
                         value={query}
                     />
                     <div className='form-search-icon'>
-                        <svg
-                            className='svg-inline--fa fa-search fa-w-16'
-                            fill='#ccc'
-                            aria-hidden='true'
-                            data-prefix='fas'
-                            data-icon='search'
-                            role='img'
-                            xmlns='http://www.w3.org/2000/svg'
-                            viewBox='0 0 512 512'>
-                            <path d='M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z'></path>
-                        </svg>
+                        {/* SVG code */}
                     </div>
                 </div>
             </div>
@@ -46,14 +46,14 @@ const SearchForm = (props) => {
                     <button
                         className='btn-delete'
                         type='button'
-                        onClick={handlerClickReset}
+                        onClick={handleClickReset}
                     >
                         RESET
                     </button>
                     <button
                         className='btn-search'
                         type='button'
-                        onClick={handlerClickSearch}
+                        onClick={handleClickSearch}
                     >
                         SEARCH
                     </button>
