f75584abaac7ee778176ff6d2d6ceec625e49ef5
dquangxyz <dq2209@gmail.com>
2023-06-28 15:59:09 +0700

    optimize search form by using useCallback, combine states, add handleSubmit form

--- src/components/search/SearchForm.jsx
+++ src/components/search/SearchForm.jsx
@@ -1,69 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import './SearchForm.css'
 
-const SearchForm = (props) => {
-    const [keyword, setKeyword] = useState("")
-    const [genre, setGenre] = useState("")
+const SearchForm = ({ onReceiveQuery }) => {
+    // Local state to capture input
+    const [formValues, setFormValues] = useState({
+        keyword: "",
+        genre: ""
+    });
 
     // Handler functions
-    const handleClickReset = () => {
-        setKeyword("")
-        setGenre("")
-        props.onReceiveQuery({ keyword: "", genre: "" })
-    }
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues((prevValues) => ({
+            ...prevValues,
+            [name]: value
+        }));
+    };
 
-    const handleClickSearch = () => {
-        props.onReceiveQuery({ keyword, genre })
-    }
+    const handleSubmit = useCallback((e) => {
+        e.preventDefault();
+        onReceiveQuery(formValues);
+    },[formValues, onReceiveQuery]);
 
-    const handleKeyDown = (e) => {
-        if (e.key === "Enter") {
-            e.preventDefault(); // Prevent form submission
-            handleClickSearch();
-        }
-    }
+    const handleReset = () => {
+        setFormValues({ keyword: "", genre: "" });
+        onReceiveQuery({ keyword: "", genre: "" });
+    };
 
+    // Also handle submit when pressing "Enter" (event from document)
+    useEffect(() => {
+        const handleEnterKeyPress = (e) => {
+            if (e.key === "Enter") {
+                e.preventDefault();
+                handleSubmit(e);
+            }
+        };
+        document.addEventListener("keydown", handleEnterKeyPress);
+        return () => {
+            document.removeEventListener("keydown", handleEnterKeyPress);
+        };
+    }, [handleSubmit]);
+
+    
     return (
-        <form id="search-form">
-            <div className='form-upper'>
-                <div className='form-input-text'>
-                    <input
-                        type='text'
-                        placeholder='Type Keywords'
-                        onChange={(e) => setKeyword(e.target.value)}
-                        onKeyDown={handleKeyDown}
-                        value={keyword}
-                    />
-                    <input
-                        type='text'
-                        placeholder='Type Genre'
-                        onChange={(e) => setGenre(e.target.value)}
-                        onKeyDown={handleKeyDown}
-                        value={genre}
-                    />
-                    
-                </div>
+    <form id="search-form" onSubmit={handleSubmit}>
+        <div className="form-upper">
+            <div className="form-input-text">
+                <input
+                    type="text"
+                    placeholder="Type Keywords"
+                    name="keyword"
+                    onChange={handleInputChange}
+                    value={formValues.keyword}
+                />
+                <input
+                    type="text"
+                    placeholder="Type Genre"
+                    name="genre"
+                    onChange={handleInputChange}
+                    value={formValues.genre}
+                />
             </div>
-            <div className='form-lower'>
-                <div className='form-group-btn'>
-                    <button
-                        className='btn-delete'
-                        type='button'
-                        onClick={handleClickReset}
-                    >
-                        RESET
-                    </button>
-                    <button
-                        className='btn-search'
-                        type='button'
-                        onClick={handleClickSearch}
-                    >
-                        SEARCH
-                    </button>
-                </div>
+        </div>
+        <div className="form-lower">
+            <div className="form-group-btn">
+                <button className="btn-delete" type="button" onClick={handleReset}>
+                    RESET
+                </button>
+                <button className="btn-search" type="submit">
+                    SEARCH
+                </button>
             </div>
-        </form>
-    )
-}
-
+        </div>
+    </form>
+    );
+};
+  
+  
+  
 export default SearchForm
